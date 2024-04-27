1d6f52e07e34353fd41dc1f006fbfea2c86ef081
Majo <git@maxoverlack.dev>
2024-04-27 21:49:41 +0200

    Change name, description and favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'DMT-Blog';
+export const SITE_DESCRIPTION =
+	'Mein Blog für den Kurs Digital-Media-Technologien der Hochschule Rhein-Waal.';
